d2272eb172c999c698ecb9fdd3b1d40a1859e52b
NicklasSchlitt <43045222+NicklasSchlitt@users.noreply.github.com>
2018-11-16 14:26:27 -0500

    test
    Rock, Paper, Scissors. AI

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picRock = new System.Windows.Forms.PictureBox();
+            this.picPaper = new System.Windows.Forms.PictureBox();
+            this.picScissors = new System.Windows.Forms.PictureBox();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picRock
+            // 
+            this.picRock.Image = ((System.Drawing.Image)(resources.GetObject("picRock.Image")));
+            this.picRock.Location = new System.Drawing.Point(12, 12);
+            this.picRock.Name = "picRock";
+            this.picRock.Size = new System.Drawing.Size(164, 150);
+            this.picRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRock.TabIndex = 0;
+            this.picRock.TabStop = false;
+            this.picRock.Click += new System.EventHandler(this.picRock_Click);
+            // 
+            // picPaper
+            // 
+            this.picPaper.Image = ((System.Drawing.Image)(resources.GetObject("picPaper.Image")));
+            this.picPaper.Location = new System.Drawing.Point(182, 12);
+            this.picPaper.Name = "picPaper";
+            this.picPaper.Size = new System.Drawing.Size(164, 150);
+            this.picPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaper.TabIndex = 1;
+            this.picPaper.TabStop = false;
+            this.picPaper.Click += new System.EventHandler(this.picPaper_Click);
+            // 
+            // picScissors
+            // 
+            this.picScissors.Image = ((System.Drawing.Image)(resources.GetObject("picScissors.Image")));
+            this.picScissors.Location = new System.Drawing.Point(352, 12);
+            this.picScissors.Name = "picScissors";
+            this.picScissors.Size = new System.Drawing.Size(164, 150);
+            this.picScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picScissors.TabIndex = 2;
+            this.picScissors.TabStop = false;
+            this.picScissors.Click += new System.EventHandler(this.picScissors_Click);
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(226, 168);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayAgain.TabIndex = 3;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Location = new System.Drawing.Point(135, 194);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(256, 50);
+            this.lblOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 258);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.picScissors);
+            this.Controls.Add(this.picPaper);
+            this.Controls.Add(this.picRock);
+            this.Name = "Form1";
+            this.Text = "Rock, Paper, Scissors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picRock;
+        private System.Windows.Forms.PictureBox picPaper;
+        private System.Windows.Forms.PictureBox picScissors;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rand = new Random();
+        string compChoice, myChoice, winner;
+        int number = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCompChoice();
+        }
+        private void getCompChoice()
+        {
+            int number = rand.Next(1, 3);
+            lblOutput.Text = "";
+            switch (number)
+            {
+                case 1:
+                    compChoice = "Rock";
+                    break;
+                case 2:
+                    compChoice = "Paper";
+                    break;
+                case 3:
+                    compChoice = "Scissors";
+                    break;
+            }
+        }
+        private void showWinner()
+        {
+            if(myChoice == "Rock" && compChoice == "Scissors")
+            {
+                lblOutput.Text = "Nice job you beat a randomized Computer.\nThe Computer chose " + "Scissors";
+            }
+            if(myChoice == "Scissors" && compChoice == "Rock")
+            {
+                lblOutput.Text = "Nice try.\nThe computer beat you by picking " + "Rock";
+            }
+            if (myChoice == "Paper" && compChoice == "Rock")
+            {
+                lblOutput.Text = "Nice job you beat a randomized Computer.\nThe Computer chose " + "Rock";
+            }
+            if (myChoice == "Rock" && compChoice == "Paper")
+            {
+                lblOutput.Text = "Nice try.\nThe computer beat you by picking " + "Paper";
+            }
+            if (myChoice == "Scissors" && compChoice == "Paper")
+            {
+                lblOutput.Text = "Nice job you beat a randomized Computer.\nThe Computer chose " + "Paper";
+            }
+            if (myChoice == "Paper" && compChoice == "Scissors")
+            {
+                lblOutput.Text = "Nice try.\nThe computer beat you by picking " + "Scissors";
+            }
+            if (myChoice == compChoice)
+            {
+                lblOutput.Text = "You and the Computer chose the Same thing.\nWhat are the chances of that";
+            }
+        }
+        private void picRock_Click(object sender, EventArgs e)
+        {
+            myChoice = "Rock";
+            showWinner();
+        }
+        private void picPaper_Click(object sender, EventArgs e)
+        {
+            myChoice = "Paper";
+            showWinner();
+        }
+        private void picScissors_Click(object sender, EventArgs e)
+        {
+            myChoice = "Scissors";
+            showWinner();
+        }
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            getCompChoice();
+        }
+    }
+}
